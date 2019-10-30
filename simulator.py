706a955aeeb79d1c8712d2cde2d7de9776f70cb4
j_z129 <j_z129@txstate.edu>
2019-10-29 21:52:33 -0500

    Add files via upload

--- simulator.py
+++ simulator.py
@@ -9,10 +9,10 @@ class State:
     cycle = 1
     R = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-    def __init__(self, opcode, dataval, address, arg1, arg2, arg3, numInstructs, opcodeStr, arg1Str, arg2Str, arg3Str):
-        self.opcode = opcode
+    def __init__(self, opcodes, dataval, addrs, arg1, arg2, arg3, numInstructs, opcodeStr, arg1Str, arg2Str, arg3Str):
+        self.opcode = opcodes
         self.dataval = dataval
-        self.address = address
+        self.address = addrs
         self.numInstructions = numInstructs
         self.arg1 = arg1
         self.arg2 = arg2
@@ -27,8 +27,8 @@ class State:
         return int(index)
 
     def incrementPC(self):
-        PC = self.PC + 4
-        return PC
+        self.PC += 4
+        return self.PC
 
     def printState(self):
 
@@ -36,38 +36,40 @@ class State:
 
         with open(outputFileName + "_sim.txt", "a") as outFile:
 
-            j = self.getIndexOfMemAddress(self.PC)
+            i = self.getIndexOfMemAddress(self.PC)
             outFile.write("====================\n")
             outFile.write(
-                "cycle:" + str(self.cycle) + "\t" + str(self.PC) + "\t" + self.opcodeStr[j] + "\t" + self.arg1Str[j]
-                + self.arg2Str[j] + self.arg3Str[j] + "\n")
+                "cycle:" + str(self.cycle) + "\t" + str(self.PC) + "\t" + self.opcodeStr[i] + "\t" + self.arg1Str[i]
+                + self.arg2Str[i] + self.arg3Str[i] + "\n")
             outFile.write("\n")
             outFile.write("registers:\n")
-            outStr = "r00:\t"
+
+            outStr = "r00:"
             for i in range(0, 8):
                 outStr = outStr + "\t" + str(self.R[i])
             outFile.write(outStr)
             outFile.write("\n")
 
-            outStr = "r08:\t"
+            outStr = "r08:"
             for i in range(8, 16):
                 outStr = outStr + "\t" + str(self.R[i])
             outFile.write(outStr)
             outFile.write("\n")
 
-            outStr = "r16:\t"
+            outStr = "r16:"
             for i in range(16, 24):
                 outStr = outStr + "\t" + str(self.R[i])
             outFile.write(outStr)
             outFile.write("\n")
 
-            outStr = "r24:\t"
+            outStr = "r24:"
             for i in range(24, 32):
                 outStr = outStr + "\t" + str(self.R[i])
             outFile.write(outStr)
+            outFile.write("\n")
 
-            outFile.write("\n\n")
-            outFile.write("data:\n")
+            outFile.write("\ndata:\n")
+            outStr = "\n"
             for i in range(len(self.dataval)):
 
                 if i % 8 == 0 and i != 0 or i == len(self.dataval):
@@ -80,7 +82,7 @@ class State:
                     outStr = outStr + "\t" + str(self.dataval[i])
 
             outFile.write(outStr + "\n")
-            outFile.close()
+        outFile.close()
 
 
 class Simulator:
@@ -119,77 +121,85 @@ class Simulator:
                 continue  # go back to top
 
             elif self.opcode[i] == 1112:  # ADD OPCODE
-                self.arg3[i] = self.arg2[i] + self.arg1[i]
+                armState.R[self.arg3[i]] = armState.R[self.arg2[i]] + armState.R[self.arg1[i]]
                 armState.printState()
                 armState.incrementPC()
                 armState.cycle += 1
                 continue  # go back to top
 
             elif self.opcode[i] == 1624:  # SUB OPCODE
-                self.arg3[i] = self.arg1[i] - self.arg2[i]
+                armState.R[self.arg3[i]] = armState.R[self.arg1[i]] - armState.R[self.arg2[i]]
                 armState.printState()
                 armState.incrementPC()
                 armState.cycle += 1
                 continue  # go back to top
 
             elif self.opcode[i] == 1104:  # AND OPCODE
-                self.arg3[i] = self.arg2[i] & self.arg1[i]
+                armState.R[self.arg3[i]] = armState.R[self.arg2[i]] & armState.R[self.arg1[i]]
                 armState.printState()
                 armState.incrementPC()
                 armState.cycle += 1
                 continue  # go back to top
 
             elif self.opcode[i] == 1360:  # ORR OPCODE
-                self.arg3[i] = self.arg2[i] | self.arg1[i]
+                armState.R[self.arg3[i]] = armState.R[self.arg2[i]] | armState.R[self.arg1[i]]
                 armState.printState()
                 armState.incrementPC()
                 armState.cycle += 1
                 continue  # go back to top
 
             elif self.opcode[i] == 1872:  # EOR OPCODE
-                self.arg3[i] = self.arg2[i] ^ self.arg1[i]
+                armState.R[self.arg3[i]] = armState.R[self.arg2[i]] ^ armState.R[self.arg1[i]]
                 armState.printState()
                 armState.incrementPC()
                 armState.cycle += 1
                 continue  # go back to top
 
             elif self.opcode[i] == 1691:  # LSL OPCODE
-                self.arg3[i] = self.arg1[i] << self.arg2[i]
+                armState.R[self.arg3[i]] = armState.R[self.arg1[i]] << armState.R[self.arg2[i]]
                 armState.printState()
                 armState.incrementPC()
                 armState.cycle += 1
                 continue  # go back to top
 
             elif self.opcode[i] == 1690:  # LSR OPCODE
-                self.arg3[i] = self.arg1[i] >> self.arg2[i]
+                armState.R[self.arg3[i]] = armState.R[self.arg1[i]] >> armState.R[self.arg2[i]]
                 armState.printState()
                 armState.incrementPC()
                 armState.cycle += 1
                 continue  # go back to top
 
             elif self.opcode[i] == 1692:  # ASR OPCODE
-                self.arg3[i] = self.arg1[i] / (2*self.arg2[i])
+                armState.R[self.arg3[i]] = armState.R[self.arg1[i]] / (2 * self.arg2[i])
                 armState.printState()
                 armState.incrementPC()
                 armState.cycle += 1
                 continue  # go back to top
 
             elif 160 <= self.opcode[i] <= 191:  # B
-                jumpAddr += ((self.arg1[i] * 4) - 4)  # -4 takes care on incrementing the PC later
                 armState.printState()
+                jumpAddr += ((self.arg1[i] * 4) - 4)  # -4 takes care on incrementing the PC later
+                armState.PC = jumpAddr
                 armState.incrementPC()
                 armState.cycle += 1
                 continue  # go back to top
 
             elif 1160 <= self.opcode[i] <= 1161:  # ADDI OPCODE RANGE
-                self.arg1[i] = self.arg2[i] + self.arg3[i]
+                armState.R[self.arg1[i]] = armState.R[self.arg2[i]] + self.arg3[i]
                 armState.printState()
                 armState.incrementPC()
                 armState.cycle += 1
                 continue  # go back to top
 
             elif 1672 <= self.opcode[i] <= 1673:  # SUBI OPCODE RANGE
-                self.arg1[i] = self.arg2[i] - self.arg3[i]
+                armState.R[self.arg1[i]] = armState.R[self.arg2[i]] - self.arg3[i]
+                armState.printState()
+                armState.incrementPC()
+                armState.cycle += 1
+                continue  # go back to top
+
+            elif self.opcode[i] == 1984:  # STUR OPCODE
+                armState.dataval[self.arg1[i]] = armState.R[self.arg3[i]]
                 armState.printState()
                 armState.incrementPC()
                 armState.cycle += 1
@@ -197,8 +207,9 @@ class Simulator:
 
             elif 1440 <= self.opcode[i] <= 1447:  # CBZ OPCODE RANGE
                 if self.arg2[i] == 0:
-                    jumpAddr += ((self.arg1[i] * 4) - 4)
                     armState.printState()
+                    jumpAddr += ((self.arg1[i] * 4) - 4)
+                    armState.PC = jumpAddr
                     armState.incrementPC()
                     armState.cycle += 1
                     continue  # go back to top
@@ -210,8 +221,9 @@ class Simulator:
 
             elif 1448 <= self.opcode[i] <= 1455:  # CBNZ OPCODE RANGE
                 if self.arg2[i] != 0:
-                    jumpAddr += ((self.arg1[i] * 4) - 4)
                     armState.printState()
+                    jumpAddr += ((self.arg1[i] * 4) - 4)
+                    armState.PC = jumpAddr
                     armState.incrementPC()
                     armState.cycle += 1
                     continue  # go back to top
@@ -222,14 +234,14 @@ class Simulator:
                     continue  # go back to top
 
             elif 1940 <= self.opcode[i] <= 1943:  # MOVK OPCODE RANGE
-                self.arg3[i] = self.arg2[i] << self.arg1[i]
+                armState.R[self.arg3[i]] = armState.R[self.arg2[i]] << self.arg1[i]
                 armState.printState()
                 armState.incrementPC()
                 armState.cycle += 1
                 continue  # go back to top
 
             elif 1684 <= self.opcode[i] <= 1687:  # MOVZ OPCODE RANGE
-                self.arg3[i] = self.arg2[i] << self.arg1[i]
+                armState.R[self.arg3[i]] = armState.R[self.arg2[i]] << self.arg1[i]
                 armState.printState()
                 armState.incrementPC()
                 armState.cycle += 1
@@ -243,7 +255,7 @@ class Simulator:
 
                 print("IN SIM -- UNKNOWN INSTRUCTION ------------------- !!!!")
 
-                armState.printState()
-                armState.PC = jumpAddr
-                armState.incrementPC()
-                armState.cycle += 1
+            armState.printState()
+            armState.PC = jumpAddr
+            armState.incrementPC()
+            armState.cycle += 1
